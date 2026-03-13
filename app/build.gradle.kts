160f1c8100d2e3fc68a91803831fa967b5341b9d
maapin <maapin@taltech.ee>
2026-03-13 23:37:43 +0200

    Moved the kotlin {} block outside of the android {} block in app/build.gradle.kts as in AGP 9.0, the kotlin {} block is no longer part of the android {} extension. It is now a top-level extension provided by the Kotlin Gradle plugin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-        }
-    }
-
     buildFeatures {
         compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 }
 
