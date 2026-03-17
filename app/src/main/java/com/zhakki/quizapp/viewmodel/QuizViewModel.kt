6d04f85b59774d4b64316510d73dc64c47b92dc6
maapin <maapin@taltech.ee>
2026-03-17 23:25:40 +0200

    Added selectedCategory to viewmodel that will be used by getQuestions

--- app/src/main/java/com/zhakki/quizapp/viewmodel/QuizViewModel.kt
+++ app/src/main/java/com/zhakki/quizapp/viewmodel/QuizViewModel.kt
@@ -4,12 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zhakki.quizapp.data.model.Category
 import com.zhakki.quizapp.data.repository.QuizRepository
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class QuizViewModel(private val repository: QuizRepository) : ViewModel() {
 
     val categories: StateFlow<List<Category>> = repository.categories
+
+    private val _selectedCategory = MutableStateFlow<Category?>(null)
+    val selectedCategory: StateFlow<Category?> = _selectedCategory.asStateFlow()
 
     init {
         fetchCategories()
@@ -21,9 +26,13 @@ class QuizViewModel(private val repository: QuizRepository) : ViewModel() {
         }
     }
 
+    fun selectCategory(category: Category) {
+        _selectedCategory.value = category
+    }
+
     fun getQuestions(amount: Int) {
         viewModelScope.launch {
-            repository.getQuestions(amount)
+            repository.getQuestions(amount, category = _selectedCategory.value?.id)
         }
     }
 }
