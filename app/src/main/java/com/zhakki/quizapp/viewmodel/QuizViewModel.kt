984d7b9f0ed1b5945e2421d07d42eef25d37eca1
maapin <maapin@taltech.ee>
2026-03-17 23:53:36 +0200

    Added enum class Difficulty with values for EASY, MEDIUM, and HARD to be used as difficulty selector for getQuestions and added viewmodel state to be used by the same function

--- app/src/main/java/com/zhakki/quizapp/viewmodel/QuizViewModel.kt
+++ app/src/main/java/com/zhakki/quizapp/viewmodel/QuizViewModel.kt
@@ -3,6 +3,7 @@ package com.zhakki.quizapp.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zhakki.quizapp.data.model.Category
+import com.zhakki.quizapp.data.model.Difficulty
 import com.zhakki.quizapp.data.repository.QuizRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,6 +16,12 @@ class QuizViewModel(private val repository: QuizRepository) : ViewModel() {
 
     private val _selectedCategory = MutableStateFlow<Category?>(null)
     val selectedCategory: StateFlow<Category?> = _selectedCategory.asStateFlow()
+
+    private val _selectedDifficulty = MutableStateFlow<Difficulty?>(Difficulty.MEDIUM)
+    val selectedDifficulty: StateFlow<Difficulty?> = _selectedDifficulty.asStateFlow()
+
+    private val _amount = MutableStateFlow(10)
+    val amount: StateFlow<Int> = _amount.asStateFlow()
 
     init {
         fetchCategories()
@@ -30,9 +37,21 @@ class QuizViewModel(private val repository: QuizRepository) : ViewModel() {
         _selectedCategory.value = category
     }
 
-    fun getQuestions(amount: Int) {
+    fun selectDifficulty(difficulty: Difficulty?) {
+        _selectedDifficulty.value = difficulty
+    }
+
+    fun updateAmount(newAmount: Int) {
+        _amount.value = newAmount
+    }
+
+    fun getQuestions() {
         viewModelScope.launch {
-            repository.getQuestions(amount, category = _selectedCategory.value?.id)
+            repository.getQuestions(
+                amount = _amount.value,
+                category = _selectedCategory.value?.id,
+                difficulty = _selectedDifficulty.value?.apiValue
+            )
         }
     }
 }
